073b7f60f1e7751e80e98b638161ede60f836566
EC2 Default User <ec2-user@ip-172-31-27-154.ec2.internal>
2026-04-11 06:07:58 +0000

    change request

--- services/user-service/test.js
+++ services/user-service/test.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("User Service Running");
+  res.send("User Service v2");
 });
 
 test("GET / should return message", async () => {
